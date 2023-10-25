f65cf644668d11a5e78ede63d17b2579542a1b3f
Ligras <148986326+Ligras@users.noreply.github.com>
2023-10-25 11:22:18 -0300

    Add files via upload

---
+++ libraslingo2510/script.js
@@ -0,0 +1,12 @@
+function cadastro(){
+    document.location = "Tela_Cadastro/index.html";
+}
+function login(){
+    document.location = "Tela_Login/index.html";
+}
+function voltar(){
+    document.location = "../index.html";
+}
+function jogar(){
+    document.location = "Tela_Jogo/index.html";
+}
